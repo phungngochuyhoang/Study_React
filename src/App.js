f7b3b74356585da30b6c063647dce89655252c63
hoang phung <hoang.phung@cloudconnecttech.com>
2021-05-16 11:22:20 +0700

    study Effect hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+
+    console.log(count);
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" className="btn btn-danger btn-lg btn-block" onClick={() => {setCount(count + 1)}}> click </button>
+      <p>Count: {count}</p>
     </div>
   );
 }
